8683f519ab15906a230ad53decb56e7c231be264
BeruzDev <albertus1291@gmail.com>
2025-09-19 19:04:25 +0200

    fetch en public

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -37,7 +37,7 @@ export const useQuestionsStore = create<State>()(
 
           fetchQuestions: async (limit: number) => {
             // Simulamos una llamada a una API => en mi caso es un archivo local
-            const res = await fetch('http://localhost:5173/data.json')
+            const res = await fetch('/data.json')
             const json = await res.json()
 
             const questions = json
